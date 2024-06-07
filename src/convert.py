462a943f1eb8b10d35963e811a723b31747d99c1
Thomas McDonald <tom@conceptcoding.co.uk>
2024-06-07 11:42:19 -0600

    refactor to objects

--- src/convert.py
+++ src/convert.py
@@ -1,5 +1,4 @@
 import csv
-import datetime
 import re
 from decimal import Decimal
 from transaction import Transaction
@@ -20,64 +19,15 @@ def convert(input_file, output_file):
     writer.writerows(output)
 
 def convert_row(row):
-  txn = Transaction(row)
+  txn = Transaction.from_row(row)
   return {
     'Currency': 'GBP',
-    'Date': map_hl_date_to_date(row['Trade date']),
-    'Event': map_hl_reference_to_event(row['Reference']),
-    'FeeTax': map_hl_data_to_feetax(txn),
-    'Price': map_hl_data_to_price(txn),
-    'Quantity': map_hl_data_to_quantity(txn),
-    'Symbol': map_hl_data_to_symbol(txn),
+    'Date': txn.date(),
+    'Event': txn.event(),
+    'FeeTax': txn.feetax(),
+    'Price': txn.price(),
+    'Quantity': txn.quantity(),
+    'Symbol': txn.symbol(),
   }
-
-def map_hl_data_to_feetax(data):
-  if re.match(r'^B\d+$', data['Reference']):
-    return (data.value() * -1) - (data.unit_cost() * data.quantity() / 100).quantize(Decimal('0.01'))
-  return 0
-
-def map_hl_data_to_symbol(data):
-  if data['Reference'] == 'MANAGE FEE' or data['Reference'] == 'Card Web':
-    return 'GBP'
-  elif re.match(r'^B\d+$', data['Reference']):
-    description = data['Description']
-    match = re.search(r'^(.*?)\s*\(', description)
-    if match:
-      return match.group(1)
-    else:
-      return None
-  else:
-    return None
-
-def map_hl_data_to_price(data):
-  if data['Reference'] == 'MANAGE FEE' or data['Reference'] == 'Card Web':
-    return 1
-  elif re.match(r'^B\d+$', data['Reference']):
-    return data['Unit cost (p)']
-  else:
-    return None
-
-def map_hl_data_to_quantity(data):
-  if data['Reference'] == 'MANAGE FEE' or data['Reference'] == 'Card Web':
-    return data['Value (£)']
-  elif re.match(r'^B\d+$', data['Reference']):
-    return data['Quantity']
-  else:
-    return None
-
-
-def map_hl_reference_to_event(reference):
-  if reference == 'MANAGE FEE':
-    return 'Fee'
-  elif reference == 'Card Web':
-    return 'Cash_In'
-  elif re.match(r'^B\d+$', reference):
-    return 'Buy'
-  else:
-    raise ValueError(f"Unknown reference {reference}")
-
-def map_hl_date_to_date(date):
-  return datetime.datetime.strptime(date, '%d/%m/%Y').strftime('%Y-%m-%d')
-
 
 convert('input/portfolio-summary (1).csv', 'output/portfolio-summary.csv')
--- src/transaction.py
+++ src/transaction.py
@@ -1,3 +1,5 @@
+import datetime
+import re
 from decimal import Decimal
 
 class Transaction:
@@ -7,6 +9,20 @@ class Transaction:
   def __getitem__(self, key):
     return self.row[key]
 
+  @classmethod
+  def from_row(cls, row):
+    reference = row['Reference']
+    if reference == 'MANAGE FEE':
+      return FeeTransaction(row)
+    elif reference == 'Card Web':
+      return CashInTransaction(row)
+    elif re.match(r'^B\d+$', reference):
+      return BuyTransaction(row)
+
+  def date(self):
+    date = self.row['Trade date']
+    return datetime.datetime.strptime(date, '%d/%m/%Y').strftime('%Y-%m-%d')
+
   def quantity(self):
     return Decimal(self.row['Quantity'])
 
@@ -15,3 +31,63 @@ class Transaction:
 
   def value(self):
     return Decimal(self.row['Value (£)'])
+
+class CashInTransaction(Transaction):
+  def __init__(self, row):
+    super().__init__(row)
+
+  def event(self):
+    return 'Cash_In'
+
+  def feetax(self):
+    return 0
+
+  def price(self):
+    return 1
+
+  def quantity(self):
+    return self.value()
+
+  def symbol(self):
+    return 'GBP'
+
+class FeeTransaction(Transaction):
+  def __init__(self, row):
+    super().__init__(row)
+
+  def event(self):
+    return 'Fee'
+
+  def feetax(self):
+    return self.value() * -1
+
+  def price(self):
+    return 0
+
+  def quantity(self):
+    return 0
+
+  def symbol(self):
+    return 'GBP'
+
+class BuyTransaction(Transaction):
+  def __init__(self, row):
+    super().__init__(row)
+
+  def event(self):
+    return 'Buy'
+
+  def feetax(self):
+    return (self.value() * -1) - (self.unit_cost() * self.quantity() / 100).quantize(Decimal('0.01'))
+
+  def price(self):
+    # todo: may need special casing for UK / non-UK stocks
+    return self.unit_cost() / 100
+
+  def symbol(self):
+    description = self['Description']
+    match = re.search(r'^(.*?)\s*\(', description)
+    if match:
+      return match.group(1)
+    else:
+      raise ValueError(f"Could not extract symbol from description {description}")
