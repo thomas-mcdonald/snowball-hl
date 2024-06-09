abe8071183ee5729dabb099890dd267ab13fb099
Thomas McDonald <tom@conceptcoding.co.uk>
2024-06-08 18:14:30 -0600

    validate all files in input before running conversion

--- src/convert.py
+++ src/convert.py
@@ -5,15 +5,17 @@ from decimal import Decimal
 from transaction import ShareTransaction, Transaction
 import os
 
-def convert(input_file, output_file, config):
+def load_file(filename):
   with open(input_file, 'r', encoding='ISO-8859-1') as f:
     # HL exports have 5 lines of metadata before the actual data
     for _ in range(5):
       f.readline()
     reader = csv.DictReader(f)
-    data = [Transaction.from_row(row) for row in reader]
+    return [Transaction.from_row(row) for row in reader]
 
-  # Validate configuration exists for all known companies in data
+# discover any unmapped companies in the input file
+def validate_company_mapping(input_file):
+  data = load_file(input_file)
   missing_companies = set()
   for txn in data:
     if type(txn) is ShareTransaction:
@@ -21,10 +23,10 @@ def convert(input_file, output_file, config):
       company_config = txn._find_company_config(config)
       if company_config is None:
         missing_companies.add(company_string)
+  return missing_companies
 
-  if missing_companies:
-    raise ValueError(f'Could not find configuration for companies: {missing_companies}. Add mapping to config.json and run again.')
-
+def convert(input_file, output_file, config):
+  data = load_file(input_file)
   output = [convert_txn(txn) for txn in data]
 
   with open(output_file, 'w', newline='') as f:
@@ -51,7 +53,24 @@ with open('config.json', 'r') as f:
 input_dir = 'input/'
 output_dir = 'output/'
 
-for filename in os.listdir(input_dir):
+inputs = os.listdir(input_dir)
+missings = set()
+for filename in inputs:
+  input_file = os.path.join(input_dir, filename)
+  missing_companies = validate_company_mapping(input_file)
+  missings = missings.union(missing_companies)
+
+if missings:
+  json_schema = [{ 'name': c, 'isin': None, 'ticker': None, 'symbol': None } for c in missings]
+  print('The following companies are missing from configuration:')
+  for company in missings:
+    print(f'  {company}')
+  print('Add the following to config.json and configure with correct ISIN or ticker / market keys:')
+  print(json.dumps(json_schema, indent=2))
+  exit(1)
+
+
+for filename in inputs:
   input_file = os.path.join(input_dir, filename)
   output_file = os.path.join(output_dir, filename)
   convert(input_file, output_file, config)
